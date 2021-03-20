81d679da92b944d2cc299fb9c2b2fe38cfde542b
Hardil Mehta <mehtahardil@gmail.com>
2021-03-20 17:59:24 -0400

    Added Nested Class to format message and implemented locale support

--- src/labs/pm/data/ProductManager.java
+++ src/labs/pm/data/ProductManager.java
@@ -30,6 +30,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 /**
  *
@@ -37,36 +38,42 @@ import java.util.ResourceBundle;
  */
 public class ProductManager {
 
-    private Locale locale;
-    private ResourceBundle resources;
-    private DateTimeFormatter dateFormat;
-    private NumberFormat moneyFormat;
     private Map<Product, List<Review>> products = new HashMap<>();
-
+    private ResourceFormatter formatter;
+    
+    private static Map<String, ResourceFormatter> formatters = Map.of("en-GB", new ResourceFormatter(Locale.UK), "en-US", new ResourceFormatter(Locale.US), "fr-FR", new ResourceFormatter(Locale.FRANCE), "ru-RU", new ResourceFormatter(new Locale("ru","RU")),"zh-CN", new ResourceFormatter(Locale.CHINA));  
+    
 //    private Product product;
 //    private Review reviews[] = new Review[5];
-
+    public ProductManager(String languageTag) {
+        changeLocale(languageTag);
+    }
     public ProductManager(Locale locale) {
-        this.locale = locale;
-        resources = ResourceBundle.getBundle("labs.pm.data.resources", locale);
-        dateFormat = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).localizedBy(locale);
-        moneyFormat = NumberFormat.getCurrencyInstance(locale);
+        this(locale.toLanguageTag());
+    }
+    
+    public void changeLocale(String languageTag){
+        formatter = formatters.getOrDefault(languageTag, formatters.get("en-GB"));
+    }
+    
+    public static Set<String> getSupportedLocale(){
+        return formatters.keySet();
     }
 
     public Product createProduct(int id, String name, BigDecimal price, Rating rating, LocalDate bestBefore) {
         Product product = new Food(id, name, price, rating, bestBefore);
-        products.putIfAbsent(product,new ArrayList<>());
+        products.putIfAbsent(product, new ArrayList<>());
         return product;
     }
 
     public Product createProduct(int id, String name, BigDecimal price, Rating rating) {
         Product product = new Drink(id, name, price, rating);
-        products.putIfAbsent(product,new ArrayList<>());
-         return product;
+        products.putIfAbsent(product, new ArrayList<>());
+        return product;
     }
-    
+
     public Product reviewProduct(int id, Rating rating, String comments) {
-        return reviewProduct(findProduct(id),rating, comments);
+        return reviewProduct(findProduct(id), rating, comments);
     }
 
     public Product reviewProduct(Product product, Rating rating, String comments) {
@@ -77,7 +84,7 @@ public class ProductManager {
         products.remove(product, reviews);
         reviews.add(new Review(comments, rating));
         int sum = 0;
-        for(Review review: reviews){
+        for (Review review : reviews) {
             sum += review.getRating().ordinal();
         }
         product = product.applyRating(Rateable.convert(Math.round((float) sum / reviews.size())));
@@ -85,40 +92,70 @@ public class ProductManager {
         return product;
 
     }
-    
-    public Product findProduct(int id){
+
+    public Product findProduct(int id) {
         Product result = null;
-        for(Product product: products.keySet()){
-            if(product.getId() == id){
+        for (Product product : products.keySet()) {
+            if (product.getId() == id) {
                 result = product;
                 break;
             }
         }
         return result;
     }
-    
-    public void printProductReport(int id){
-        printProductReport(findProduct(id ));
+
+    public void printProductReport(int id) {
+        printProductReport(findProduct(id));
     }
 
     public void printProductReport(Product product) {
         List<Review> reviews = products.get(product);
         StringBuilder txt = new StringBuilder();
-        txt.append(MessageFormat.format(resources.getString("product"), product.getName(), moneyFormat.format(product.getPrice()), product.getRating().getStars(), dateFormat.format(product.getBestBefore())));
+        txt.append(formatter.formatProduct(product));
         txt.append('\n');
         Collections.sort(reviews);
         for (Review review : reviews) {
             if (review == null) {
                 break;
             }
-            txt.append(MessageFormat.format(resources.getString("review"), review.getRating().getStars(), review.getComments()));
+            txt.append(formatter.formatReview(review));
             txt.append('\n');
         }
         if (reviews.isEmpty()) {
-            txt.append(resources.getString("no.reviews"));
+            txt.append(formatter.getText("no.reviews"));
             txt.append("\n");
         }
 
         System.out.println(txt);
     }
+
+    private static class ResourceFormatter {
+
+        private Locale locale;
+        private ResourceBundle resources;
+        private DateTimeFormatter dateFormat;
+        private NumberFormat moneyFormat;
+
+        private ResourceFormatter(Locale locale) {
+            this.locale = locale;
+            resources = ResourceBundle.getBundle("labs.pm.data.resources", locale);
+            dateFormat = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT).localizedBy(locale);
+            moneyFormat = NumberFormat.getCurrencyInstance(locale);
+        }
+
+        private String formatProduct(Product product) {
+            return MessageFormat.format(resources.getString("product"), product.getName(), moneyFormat.format(product.getPrice()), product.getRating().getStars(), dateFormat.format(product.getBestBefore()));
+        }
+
+        private String formatReview(Review review) {
+            return MessageFormat.format(resources.getString("review"), review.getRating().getStars(), review.getComments());
+        }
+
+        private String getText(String key) {
+            return resources.getString(key);
+        }
+        
+    
+    }
+
 }
