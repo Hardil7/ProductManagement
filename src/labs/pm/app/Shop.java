1b7bc37abe64f61b91c3de5e1f7cc3033c3a8309
Hardil Mehta <mehtahardil@gmail.com>
2021-03-26 17:53:00 -0400

    Changed functionality to bulk load data

--- src/labs/pm/app/Shop.java
+++ src/labs/pm/app/Shop.java
@@ -37,53 +37,18 @@ public class Shop {
      */
     public static void main(String[] args) {
         ProductManager pm = new ProductManager("en-GB");
-        // TODO code application logic here
-        pm.parseProduct("D,101,Tea,1.99,0,2021-05-23");
-        pm.printProductReport(101);        
-        pm.parseReview("101,4,Nice hot cup of tea");
-        pm.parseReview("101,2,Rather weak tea");
-        pm.parseReview("101,4,Fine Tea");
-        pm.parseReview("101,4,Good Tea");
-        pm.parseReview("101,5,Perfect Tea");
-        pm.parseReview("101,3,Just add some lemon");
         pm.printProductReport(101);
-        
-//        pm.changeLocale("ru-RU");
+        pm.createProduct(108, "Kombucha", BigDecimal.valueOf(1.99), Rating.NOT_RATED);
+        pm.reviewProduct(108, Rating.TWO_STAR, "Looks like tea, but is it?");
+        pm.reviewProduct(108, Rating.FOUR_STAR, "Fine Tea");
+        pm.reviewProduct(108, Rating.FOUR_STAR, "This is not tea");
+        pm.reviewProduct(108, Rating.FIVE_STAR, "Perfect!");
+        pm.printProductReport(108);
+//        Comparator<Product> ratingSorter = (p1, p2) -> p2.getRating().ordinal() - p1.getRating().ordinal();
+//        Comparator<Product> priceSorter = (p1, p2) -> p2.getPrice().compareTo(p1.getPrice());
+        pm.printProducts((p) -> p.getPrice().floatValue() < 2, (p1,p2) -> p2.getRating().ordinal() -p1.getRating().ordinal());
+        pm.getDiscounts().forEach((rating, discount) -> System.out.println(rating + "\t" + discount));
 
-        pm.parseProduct("D,102,Coffee,1.99,0,2021-05-23");
-        pm.parseReview("102,3,Coffee was ok");
-        pm.parseReview("102,1,where is the milk");
-        pm.parseReview("102,5,It's perfect with ten spoons of sugar");
-        pm.printProductReport(102);
-//
-        pm.parseProduct("F,103,Cake,3.99,0,2021-03-15");
-        pm.parseReview("103,5,Very nice cake");
-        pm.parseReview("103,4,It's very good, but i expected more chocolate");
-        pm.parseReview("103,5,Cake's perfect");
-        pm.printProductReport(103);
-
-        pm.parseProduct("F,104,Cookie,2.99,0,2021-03-25");
-        pm.parseReview("104,3,Just another cookie");
-        pm.parseReview("104,3,Ok");
-        pm.printProductReport(104);
-
-        pm.parseProduct("D,105,Hot Chocolate,2.50,0,2021-05-23");
-        pm.parseReview("105,4,Tasty");
-        pm.parseReview("105,4,Not bad at all");
-        pm.printProductReport(105);
-
-        pm.parseProduct("D,106,Chocolate,2.50,0,2021-03-25");
-        pm.parseReview("106,2,Too Sweet");
-        pm.parseReview("106,3,Better than cookie");
-        pm.parseReview("106,2,Too bitter");
-        pm.parseReview("106,1,I don't get it!");
-        pm.printProductReport(106);
-        
-//        Comparator<Product> ratingSorter = (p1,p2) -> p2.getRating().ordinal() - p1.getRating().ordinal();
-//        Comparator<Product> priceSorter = (p1,p2) -> p2.getPrice().compareTo(p1.getPrice());
-//        pm.printProducts( (p) -> p.getPrice().floatValue() < 2, ratingSorter);
-//        pm.getDiscounts().forEach((rating,discount) -> System.out.println(rating+"\t"+discount));
-//        pm.printProducts(priceSorter);
     }
 
 }
